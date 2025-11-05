b5ac6bdcd60339bb417972c29a1a44233a2bb2fc
Chris Nowicki <chris.nowicki@bigcommerce.com>
2025-11-05 10:40:48 -0500

    update icon import for uses

--- src/data/uses.ts
+++ src/data/uses.ts
@@ -7,52 +7,52 @@ type SoftwareDataItem = {
 export const softwareData: SoftwareDataItem[] = [
   {
     title: '1Password',
-    imageSrc: '/1password.png',
+    imageSrc: '/icons/1password.png',
     link: 'https://1password.com/',
   },
   {
     title: 'AltTab',
-    imageSrc: '/alttab.png',
+    imageSrc: '/icons/alttab.png',
     link: 'https://alt-tab-macos.netlify.app/',
   },
   {
     title: 'Bear Notes',
-    imageSrc: '/bear.png',
+    imageSrc: '/icons/bear.png',
     link: 'https://bear.app/',
   },
   {
     title: 'CleanShot X',
-    imageSrc: '/cleanshotx.png',
+    imageSrc: '/icons/cleanshotx.png',
     link: 'https://cleanshot.com/',
   },
   {
     title: 'Cursor',
-    imageSrc: '/cursor.png',
+    imageSrc: '/icons/cursor.png',
     link: 'https://cursor.com',
   },
   {
     title: 'Obsidian',
-    imageSrc: '/obsidian.png',
+    imageSrc: '/icons/obsidian.png',
     link: 'https://obsidian.md/',
   },
   {
     title: 'Postman',
-    imageSrc: '/postman.png',
+    imageSrc: '/icons/postman.png',
     link: 'https://www.postman.com/',
   },
   {
     title: 'RayCast',
-    imageSrc: '/raycast.png',
+    imageSrc: '/icons/raycast.png',
     link: 'https://www.raycast.com/',
   },
   {
     title: 'VS Code',
-    imageSrc: '/vscode.png',
+    imageSrc: '/icons/vscode.png',
     link: 'https://code.visualstudio.com/',
   },
   {
     title: 'Wezterm',
-    imageSrc: '/wezterm.png',
+    imageSrc: '/icons/wezterm.png',
     link: 'https://wezterm.org',
   }
 ]
